d2d56e04df3b66620c8c3fe741f7ece823fa62a1
Tapasya Raghuwanshi <tapasyaraghuwanshi0726@gmail.com>
2026-07-17 18:50:39 +0530

    Complete Applications module and integrate dashboard

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -6,10 +6,11 @@ export const getDashboardStats = async (req, res) => {
 
         const userId = req.user.user_id;
 
-        const result = await pool.query(
+        // Focus Time
+        const focusResult = await pool.query(
             `
             SELECT
-                COALESCE(SUM(duration),0) AS total_focus_minutes
+                COALESCE(SUM(duration), 0) AS total_focus_minutes
             FROM focus_sessions
             WHERE user_id = $1
             AND mode = 'WORK';
@@ -17,17 +18,43 @@ export const getDashboardStats = async (req, res) => {
             [userId]
         );
 
+        // Application Statistics
+        const applicationResult = await pool.query(
+            `
+            SELECT
+                COUNT(*) AS total_applications,
+
+                COUNT(*) FILTER (
+                    WHERE status = 'Interview'
+                ) AS interviews
+
+            FROM applications
+            WHERE user_id = $1;
+            `,
+            [userId]
+        );
+
         return res.status(200).json({
-            totalFocusMinutes: Number(result.rows[0].total_focus_minutes)
+
+            totalFocusMinutes:
+                Number(focusResult.rows[0].total_focus_minutes),
+
+            totalApplications:
+                Number(applicationResult.rows[0].total_applications),
+
+            interviews:
+                Number(applicationResult.rows[0].interviews)
+
         });
 
     } catch (error) {
 
-    console.error("Dashboard Error:", error);
+        console.error("Dashboard Error:", error);
 
-    return res.status(500).json({
-        message: error.message
-    });
+        return res.status(500).json({
+            message: error.message
+        });
 
-}
+    }
+
 };
