2978234717c0cb6842e7b91e029b089222647b5a
Tapasya Raghuwanshi <tapasyaraghuwanshi0726@gmail.com>
2026-07-24 12:38:14 +0530

    final changes made

--- config/db.js
+++ config/db.js
@@ -7,17 +7,12 @@ const { Pool, types } = pg;
 
 types.setTypeParser(1082, (value) => value);
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_HOST:", process.env.DB_HOST);
-
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL || undefined,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    connectionTimeoutMillis: 30000
 });
 
 pool.connect()
